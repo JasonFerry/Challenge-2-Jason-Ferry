0e65552c41766fe00a5e984612f802df6c46bdd1
JasonFerry <43120122+JasonFerry@users.noreply.github.com>
2018-10-21 21:25:16 -0400

    First build of the game

---
+++ Challenge 2/Assets/Scripts/CharacterAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnim : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)){
+            anim.SetBool("IsRunning", true);
+        } else {
+            anim.SetBool("IsRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("jump");
+        }
+    }
+}
---
+++ Challenge 2/Assets/Scripts/Player_Move_Prot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move_Prot : MonoBehaviour
+{
+
+    public int playerSpeed = 10;
+    private bool facingRight = false;
+    public int playerJumpPower = 1250;
+    private float moveX;
+    public bool isGrounded;
+    public float distanceToBottomOfPlayer = 0.9f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        PlayerMove();
+        PlayerRaycast();
+    }
+
+    void PlayerMove()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Jump();
+        }
+        //ANIMATION
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        //PHYSICS   
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        //JUMPING CODE
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+        isGrounded = false;
+    }
+
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    void PlayerRaycast()
+    {
+        RaycastHit2D rayUp = Physics2D.Raycast(transform.position, Vector2.up);
+        if (rayUp != null && rayUp.collider != null && rayUp.distance < distanceToBottomOfPlayer && rayUp.collider.name == "Box_2")
+        {
+            Destroy (rayUp.collider.gameObject);
+        }
+
+
+            RaycastHit2D rayDown = Physics2D.Raycast(transform.position, Vector2.down);
+        if (rayDown != null && rayDown.collider != null && rayDown.distance < distanceToBottomOfPlayer && rayDown.collider.tag == "enemy")
+        {
+            GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 1000);
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * 200);
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 10;
+            rayDown.collider.gameObject.GetComponent<Rigidbody2D>().freezeRotation = false;
+            rayDown.collider.gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+            rayDown.collider.gameObject.GetComponent<EnemyMove> ().enabled = false;
+        }
+        if (rayDown != null && rayDown.collider != null && rayDown.distance < distanceToBottomOfPlayer && rayDown.collider.tag != "enemy")
+        {
+            isGrounded = true;
+        }
+    }
+}
